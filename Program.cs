8664831916c9bbb42ad37a1dc37bfeb5d27ecbf6
aweeshashim <30947186+aweeshashim@users.noreply.github.com>
2022-09-21 09:44:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mySystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public  static string UserType;
+        public static string SalesMan;
+        public static DateTime VALIDITION_DATE;
+        public static string UserID;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PL.FRM_SALES());
+        }
+    }
+}
